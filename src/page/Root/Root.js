ee441de4f19be11f1eb475c26eac52bbe3bc58a0
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-01-28 22:27:32 +0100

    Added formContact component

---
+++ src/page/Root/Root.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FormContact from "../../component/FormContact/FormContact";
+
+import './Root.css';
+
+class Root extends Component {
+  
+  render() {
+    return (
+      <FormContact />
+  );
+  }
+  
+};
+
+export default Root;
